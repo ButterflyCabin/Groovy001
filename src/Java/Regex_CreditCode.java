2d2e7ad90a236e26415cd93cfd0f7645dc47f863
ButterflyCabin <1014126013@qq.com>
2017-03-15 15:49:41 +0800

    修复 企业信用代码校验

--- src/Java/Regex_CreditCode.java
+++ src/Java/Regex_CreditCode.java
@@ -9,9 +9,20 @@ import java.util.Map;
  * Created by kachesiji on 2016/5/21.
  */
 class Regex_CreditCode {
-    static String creditCode1 = "91110105355299937Y";// 公司
-    static String creditCode2 = "91350100M000100Y43";// 测试
-    static String creditCode3 = "52370100MJD692273E";// 公司
+//    百度所得91350200M000000510,91350128M00000019A,G1035060200113440U待调试
+    static String[] creditCode = {
+        "91440300311906854A",
+        "91110105771583283D","91110000802100433B","91110108740084270C",
+        "112302070019030687","91110108735106340Y","911101077776591113",
+        "51610700305332460K","91440300359143307G","11610923MB2900168E",
+        "1134082855783407XL","91341000MA2N0ABGXX","91110228397176695A",
+        "91110111567495091J","51100000500019541K","11330283002982634F",
+        "92510100MA61WJ3204","52610500MJU60350X1","91510000779827684C",
+        "1142092201131905X1","91350100M000100Y43","51140200MJ1693047P",
+        "913101135696449862","91460600MA5RC30P9Y","11991000010647510A",
+        "91110113766754543Y","121526004609138637","52440111MJK9980480",
+        "91330200750364874C","123456789012345678","52120000MJ0529018E"
+};
     static String isCreditCode = "true";
     static String error_CreditCode = "社会信用代码有误";
     static String error_CreditCode_min = "社会信用代码不足18位，请核对后再输！";
@@ -22,11 +33,11 @@ class Regex_CreditCode {
     static int[] power = {1,3,9,27,19,26,16,17,20,29,25,13,8,24,10,30,28};
     static char[] code = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','T','U','W','X','Y'};
    public  static  void main(String[] args){
-        String temp = creditCode1;
+       String temp = creditCode[0];
        System.out.println(temp);
-        initDatas(code.length);
-        pre17(temp);
-        isCreditCode(temp);
+       initDatas(code.length);
+       pre17(temp);
+       isCreditCode(temp);
     }
 
     /**
@@ -47,6 +58,7 @@ class Regex_CreditCode {
         }else{
             int sum =  sum(pre17s);
             int temp = sum%31;
+            temp = temp == 0 ? 31 : temp;
             System.out.println(code[31-temp]+" "+(creditCode.substring(17,18).equals(code[31-temp]+"")?isCreditCode:error_CreditCode));
             return creditCode.substring(17,18).equals(code[31-temp]+"")?isCreditCode:error_CreditCode;
         }
@@ -84,6 +96,5 @@ class Regex_CreditCode {
         for(int i=0;i<code.length;i++){
             datas.put(code[i]+"",i);
         }
-        System.out.println();
     }
 }
